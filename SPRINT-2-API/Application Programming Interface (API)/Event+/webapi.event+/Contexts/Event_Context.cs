85f47aa529aa7bbc05c135604b91c2cea03a5c68
Vinicius Porcionato <porcionatovinicius@gmail.com>
2023-12-13 14:34:02 -0300

    EventPlus: detalhes evento

--- SPRINT-2-API/Application Programming Interface (API)/Event+/webapi.event+/Contexts/Event_Context.cs
+++ SPRINT-2-API/Application Programming Interface (API)/Event+/webapi.event+/Contexts/Event_Context.cs
@@ -28,8 +28,8 @@ namespace webapi.event_.Contexts
         /// <param name="optionsBuilder">Objeto com as configurações definidas</param>
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //optionsBuilder.UseSqlServer("Server=NOTE04-S15; Database=event+vinicius; User Id = sa; Pwd= Senai@134; TrustServerCertificate=true;");
-            optionsBuilder.UseSqlServer("Server=tcp:eventvinicius-server.database.windows.net,1433;Initial Catalog=eventViniciusDatabase;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30; User Id=eventvinicius-server; Pwd=Senai@134;");
+            optionsBuilder.UseSqlServer("Server=NOTE04-S15; Database=event+vinicius; User Id = sa; Pwd= Senai@134; TrustServerCertificate=true;");
+            //optionsBuilder.UseSqlServer("Server=tcp:eventvinicius-server.database.windows.net,1433;Initial Catalog=eventViniciusDatabase;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30; User Id=eventvinicius-server; Pwd=Senai@134;");
             base.OnConfiguring(optionsBuilder);
         }
     }
